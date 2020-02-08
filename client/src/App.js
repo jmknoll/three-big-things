cb55940155da9e360cf7c3798df71f0fbb2a735d
Jameson Knoll <j.knoll621@gmail.com>
2020-02-08 18:21:30 -0500

    Add ui lib

--- client/src/App.js
+++ client/src/App.js
@@ -5,28 +5,34 @@ import {
   Route,
   Redirect
 } from "react-router-dom";
+import styled, { ThemeProvider } from "styled-components";
 
 import Login from "./pages/Login";
 import Home from "./pages/Home";
 import NoMatch from "./pages/404";
 
+import { theme } from "@smooth-ui/core-sc";
+//import { theme } from "./ui/theme";
+
 function App() {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   return (
-    <Router>
-      <Switch>
-        <Route path="/signin">
-          <Login />
-        </Route>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/signin">
+            <Login />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,7 +1,45 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+import {
+  Form,
+  FormField,
+  FormFieldLabel,
+  Input,
+  Button
+} from "@smooth-ui/core-sc";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const Header = styled.h1``;
 
 const Login = () => {
-  return <p>Login Page</p>;
+  return (
+    <Container>
+      <Header>Sign In</Header>
+      <Form>
+        <FormField>
+          <FormFieldLabel name="password">Name</FormFieldLabel>
+          <Input name="email" placeholder="you@email.com" type="text" />
+        </FormField>
+        <FormField>
+          <FormFieldLabel name="password">Password</FormFieldLabel>
+          <Input name="password" type="password" />
+        </FormField>
+        <FormField row scale="lg">
+          <Link to="/home">
+            <Button type="submit">Submit</Button>
+          </Link>
+        </FormField>
+      </Form>
+    </Container>
+  );
 };
 
 export default Login;
