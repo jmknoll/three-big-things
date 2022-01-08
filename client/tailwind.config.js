ee5da3686242da9e5474ac2873765f97ad9ee59c
Jameson Knoll <j.knoll621@gmail.com>
2022-01-08 18:24:46 -0500

    fix tailwind styles

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,23 @@ const colors = require("tailwindcss/colors");
 const forms = require("@tailwindcss/forms");
 
 module.exports = {
-  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  purge: {
+    content: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+    options: {
+      safelist: [
+        "h-8",
+        "w-8",
+        "bg-yellow-50",
+        "bg-red-50",
+        "bg-green-50",
+        "bg-blue-50",
+        "bg-yellow-400",
+        "bg-red-400",
+        "bg-green-400",
+        "bg-blue-400",
+      ],
+    },
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
