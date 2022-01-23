4cdc32a2b973db4447f3f23026eab4b58c25371d
Jameson Knoll <j.knoll621@gmail.com>
2022-01-23 17:43:30 -0500

    migrate ORM from sequelize to prisma

--- server/src/controllers/GoalController.ts
+++ server/src/controllers/GoalController.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import { PrismaClient } from "@prisma/client";
-import moment from "moment";
-// const db = require("../models");
-// const Goal = db.Goal;
-// const User = db.User;
 
 const prisma = new PrismaClient();
+
+interface Query {
+  archived: string | undefined;
+}
 
 async function create(req: Request, res: Response) {
   if (!req.body.content) {
@@ -53,87 +53,42 @@ async function update(req: Request, res: Response) {
   }
 
   try {
-    const [status, created] = await Goal.update(
-      {
-        archived: req.body.archived,
+    const goal = await prisma.goal.update({
+      where: {
+        id: req.body.id,
+      },
+      data: {
         name: req.body.name,
         content: req.body.content,
         period: req.body.period,
         status: req.body.status,
       },
-      {
-        where: {
-          id: id,
-        },
-        returning: true,
-        raw: true,
-      }
-    );
-    res.status(200).send(created);
-  } catch (e) {
+    });
+
+    res.status(200).send(goal);
+  } catch (e: any) {
     res.status(500).send({ error: e.message || "Error creating goal." });
   }
 }
 
-function shouldArchive(record, timezone_offset) {
-  let created = moment.utc(record.createdAt);
-  let created_local = created.subtract(timezone_offset, "minutes");
-  let now_local = moment.utc().subtract(timezone_offset, "minutes");
-  if (record.period === "DAILY" && !created_local.isSame(now_local, "day")) {
-    return record.id;
-  }
-  if (record.period === "WEEKLY" && !created_local.isSame(now_local, "week")) {
-    return record.id;
-  }
+async function findAll(req: Request, res: Response) {
+  try {
+    const archived: string = JSON.parse(req.query.archived as string);
 
-  return -1;
-}
-
-async function updateGoalStatus(
-  req: Request,
-  res: Response,
-  next: NextFunction
-) {
-  next();
-  // this is for automatic archiving - might come back later
-  // const archived = JSON.parse(req.query.archived);
-  // if (!archived) {
-  //   const user = await User.findByPk(req.user_id, { raw: true });
-  //   const goals = await Goal.findAll({
-  //     where: {
-  //       user_id: req.user_id,
-  //       archived,
-  //     },
-  //     raw: true,
-  //   });
-  //   ids = goals.map((goal) => shouldArchive(goal, user.timezone_offset));
-  //   await Goal.update(
-  //     {
-  //       archived: true,
-  //     },
-  //     {
-  //       where: {
-  //         id: ids,
-  //       },
-  //     }
-  //   );
-  // }
-  // next();
-}
-
-function findAll(req: Request, res: Response) {
-  const archived = JSON.parse(req.query.archived);
-  Goal.findAll({
-    where: {
-      user_id: req.user_id,
-      archived,
-    },
-    order: [["updatedAt", "DESC"]],
-  })
-    .then((data) => res.status(201).send(data))
-    .catch((e: any) => {
-      res.status(500).send({ error: e.message });
+    const goals = await prisma.goal.findMany({
+      where: {
+        user_id: req.body.user_id,
+      },
+      orderBy: {
+        created_at: "desc",
+      },
     });
+    res.status(200).send(goals);
+  } catch (e: any) {
+    res
+      .status(500)
+      .send({ error: e.message || "Error fetching goals for user." });
+  }
 }
 
 function destroy(req: Request, res: Response) {
@@ -151,9 +106,8 @@ function destroy(req: Request, res: Response) {
 }
 
 module.exports = {
+  findAll,
   create,
   update,
-  findAll,
-  updateGoalStatus,
   destroy,
 };
